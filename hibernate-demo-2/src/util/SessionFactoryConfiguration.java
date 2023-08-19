330924506ef440cb0b4e35e704dfcf3c17f55d22
Damith Anushka <damithanushkam016@gmail.com>
2023-08-19 14:31:57 +0530

    Add entity classes

--- hibernate-demo-2/src/util/SessionFactoryConfiguration.java
+++ hibernate-demo-2/src/util/SessionFactoryConfiguration.java
@@ -4,6 +4,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import entity.Laptop;
+import entity.Student;
+
 public class SessionFactoryConfiguration {
 
     private static SessionFactoryConfiguration sessionFactoryConfiguration;
@@ -11,7 +14,7 @@ public class SessionFactoryConfiguration {
     private SessionFactory sessionFactory;
 
     private SessionFactoryConfiguration() {
-        Configuration configuration = new Configuration().configure();
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Laptop.class).addAnnotatedClass(Student.class);
 
         sessionFactory = configuration.buildSessionFactory();
     }
